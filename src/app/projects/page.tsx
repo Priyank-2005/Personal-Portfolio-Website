9cd5db40185d0d3e71ecdd3f002aeef9cf135614
Priyank-2005 <priyank.bohra.31@gmail.com>
2025-06-14 12:26:27 +0530

    Fix2

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -11,7 +11,7 @@ export default function Projects() {
           <p>
             A web-based platform to help users learn programming through interactive courses, quizzes, and a responsive UI built with ReactJS. Features a clean, modular design focused on user-friendly navigation.
           </p>
-          <a href="#">View Project</a>
+          <a href="https://github.com/Priyank-2005/CodeVerse">View Project</a>
         </div>
 
         <div className="project-card">
@@ -19,7 +19,7 @@ export default function Projects() {
           <p>
             An AI-powered system designed to help users identify potential diseases based on symptoms. Built to simplify early diagnosis using basic rule-based AI algorithms.
           </p>
-          <a href="#">View Project</a>
+          <a href="https://github.com/Priyank-2005/AI-based-Healthcare-Diagnosis-System">View Project</a>
         </div>
 
         <div className="project-card">
@@ -27,7 +27,7 @@ export default function Projects() {
           <p>
             My personal portfolio website built using Next.js and TypeScript. Includes Home, About, Projects, and Contact pages, with light/dark mode toggle and fully responsive design using external CSS.
           </p>
-          <a href="#">View Live</a>
+          <a href="https://personal-portfolio-website-xi-two.vercel.app/">View Live</a>
         </div>
       </div>
     </section>
